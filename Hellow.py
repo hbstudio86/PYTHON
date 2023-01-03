70d59c440bfbdc6663c4feff4c157c46b5539eb1
HBSTUDIO <hbstudio@naver.com>
2023-01-03 21:00:39 +0900

    파이썬 기초 훈련

--- Hellow.py
+++ Hellow.py
@@ -1,2 +1,38 @@
-print("Hello World")
-print('hello world')
+print("Hello, world!")
+print('hello, world!')
+#230103
+#comment using
+print("세미콘론사용"); #this!
+print("붙혀서 쓰기"); print("이건 되나??")
+#indentation
+#int x = 10
+if (5+5) == 10:
+    print('10')
+#code block
+if (5+5) == 10:
+    print(10)
+    print('this')
+#operator
+print(3+3) #+op
+print(2-1) #-op
+print(2*4) #*op
+print(5/2) #/op
+print(5//2) #//op
+print(5**2) #**op
+print(5%2) #%op
+#int()
+print(int(3.3));
+print(int(5/2));
+print(int('100'));
+#type()
+print(type(10)); print(type(11.1));
+type(10);   #no output
+#divmod()
+print(divmod(5,2));
+#bin oct hex ---> dec
+print(0b1101);print(0o37);print(0x1f);
+#int ---> float
+print(float(3));print(float('5'));
+x = 10
+print(type(x));
+

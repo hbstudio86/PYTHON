d227e52d5cbea436eea343ad31163eadc1831802
HBSTUDIO <hbstudio@naver.com>
2023-01-08 20:54:09 +0900

    UNIT8:논리연산자 및 비교연산자

---
+++ 000000_230108_study.py
@@ -0,0 +1,17 @@
+## UHIT8 불과 비교 논리 연산자 알아보기  ##
+#Python에는 == 와 != 비교 연산자 외
+#is 와 is not 이 있다. 이것은 객체를 비교 하는 것이다
+print(1==1) #이러면 두 값을 비교 한다
+print(1==1.0) #역시 두 값을 비교 하므로 참이다
+print(1 is 1.0) # 1이라는 객체와 1.0이라는 객체는 다르므로 거짓이다
+print("Object ID: ",id(1),"Object ID: ",id(1.0),sep="//")
+#논리 연산자
+#&&
+print(True and True)
+print(False and True)
+#||
+print(True or False)
+print("True or False = ",True or False)
+#!
+print("not True: ",not True)
+

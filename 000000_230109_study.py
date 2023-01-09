6339862223ce3a22c91105e654608c01e2a9420a
HBSTUDIO <hbstudio@naver.com>
2023-01-09 20:47:39 +0900

    UNIT9 STUDY

---
+++ 000000_230109_study.py
@@ -0,0 +1,10 @@
+## UNIT9 문자열 사용하기 ##
+Hello = 'Hello Wolrd'
+print(Hello)
+Hello2 = "Hello\nWorld!\n!!"
+print(Hello2)
+h3 = "TEST 'is Test"    #"과 '의 혼용은 문자열에서 해당 문자를 사용하기 위함
+h4 = 'Test "Just"TEST'  
+print(h3,h4)
+h5 = """hhh'kkk'tkdk"""
+print(h5)
